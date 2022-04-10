0077241bb365b5d156f11d7be22b954027d5102f
cseltzner <chase.seltz21@gmail.com>
2022-04-09 18:47:51 -0700

    Finished major calculator logic

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const btn_equals = document.getElementById("btn_equals");
 const btn_swap_sign = document.getElementById("btn_swap_sign");
 const display_bottom = document.getElementById("display_bottom");
 const display_top = document.getElementById("display_top");
+const display_wrapper = document.getElementById("display_wrapper");
+
 
 /**
  * Enum class representing current state of the calculator
@@ -26,7 +28,7 @@ const display_top = document.getElementById("display_top");
     static ADD_MODE = new OperatorMode("ADD_MODE");
     static SUBTRACT_MODE = new OperatorMode("SUBTRACT_MODE");
     static MULTIPLY_MODE = new OperatorMode("MULTIPLY_MODE");
-    static MULTIPLY_MODE = new OperatorMode("MULTIPLY_MODE");
+    static DIVIDE_MODE = new OperatorMode("DIVIDE_MODE");
 }
 
 /**
@@ -66,6 +68,7 @@ function clear() {
     updateUI()
 }
 
+
 btn_0.addEventListener('click', function() { onNumberClicked(ValueClicked.ZERO) });
 btn_1.addEventListener('click', function() { onNumberClicked(ValueClicked.ONE) });
 btn_2.addEventListener('click', function() { onNumberClicked(ValueClicked.TWO) });
@@ -78,14 +81,124 @@ btn_8.addEventListener('click', function() { onNumberClicked(ValueClicked.EIGHT)
 btn_9.addEventListener('click', function() { onNumberClicked(ValueClicked.NINE) });
 
 btn_clear.addEventListener('click', function() { clear() });
+btn_swap_sign.addEventListener('click', function() { 
+    currentValue = swapSign(currentValue);
+    updateUI();
+});
+btn_add.addEventListener('click', onAddClick);
+btn_subtract.addEventListener('click', onSubtractClick);
+btn_multiply.addEventListener('click', onMultiplyClick);
+btn_divide.addEventListener('click', onDivideClick);
+btn_equals.addEventListener('click', onEqualClicked);
+
+
+
+document.addEventListener('keydown', onKeyPress);
+window.addEventListener('resize', updateUI); // Makes sure display text is shrunk properly on resize
+
 
 function updateUI() {
     if (isOperatingValueShown) {
-        display_top.textContent = operatingValue;
+        let text = operatingValue.toString()
+        if (text.length > 9) {
+            display_top.textContent = operatingValue.toExponential(5).toString();
+        } else {
+            display_top.textContent = text;
+        }
     } else {
         display_top.textContent = "";
     }
-    display_bottom.textContent = currentValue.toString()
+    let text = currentValue.toString()
+    // Changes font size between 100% and smaller if needed
+    if (text.length > 9) {
+        display_bottom.textContent = currentValue.toExponential(5).toString();
+        
+        if (display_wrapper.offsetWidth < 400) {
+            display_bottom.style.fontSize = "75%";
+            display_top.style.fontSize = "75%";    
+        } else {
+            display_bottom.style.fontSize = "100%";
+            display_top.style.fontSize = "100%";    
+        }
+    } else {
+        display_bottom.textContent = currentValue.toString()
+        display_bottom.style.fontSize = "100%";
+        display_top.style.fontSize = "100%";
+    }
+}
+
+
+function onAddClick() {
+    performOperation(OperatorMode.ADD_MODE);
+}
+
+function onSubtractClick() {
+    performOperation(OperatorMode.SUBTRACT_MODE);
+}
+
+function onMultiplyClick() {
+    performOperation(OperatorMode.MULTIPLY_MODE);
+}
+
+
+function onDivideClick() {
+    performOperation(OperatorMode.DIVIDE_MODE);
+}
+
+function onEqualClicked() {
+    let tempCurrent = 0;
+
+    if (currentOpMode == OperatorMode.NONE) {
+        return;
+    }
+
+    if (currentOpMode == OperatorMode.ADD_MODE) {
+        tempCurrent = add(operatingValue, currentValue);
+    }
+
+    if (currentOpMode == OperatorMode.SUBTRACT_MODE) {
+        tempCurrent = subtract(operatingValue, currentValue);
+    }
+
+    if (currentOpMode == OperatorMode.MULTIPLY_MODE) {
+        tempCurrent = multiply(operatingValue, currentValue);
+    }
+
+    if (currentOpMode == OperatorMode.DIVIDE_MODE) {
+        tempCurrent = divide(operatingValue, currentValue);
+    }
+
+    clear();
+    currentValue = tempCurrent;
+    operationJustPerformed = true;
+    updateUI();
+}
+
+function performOperation(newOperatorMode) {
+    if (currentOpMode == OperatorMode.NONE) {
+        operatingValue = currentValue;
+    }
+
+    if (currentOpMode == OperatorMode.ADD_MODE) {
+        operatingValue = add(operatingValue, currentValue);
+    }
+
+    if (currentOpMode == OperatorMode.SUBTRACT_MODE) {
+        operatingValue = subtract(operatingValue, currentValue);
+    }
+
+    if (currentOpMode == OperatorMode.MULTIPLY_MODE) {
+        operatingValue = multiply(operatingValue, currentValue);
+    }
+
+    if (currentOpMode == OperatorMode.DIVIDE_MODE) {
+        operatingValue = divide(operatingValue, currentValue);
+    }
+
+    currentValue = 0;
+    isOperatingValueShown = true;
+    currentOpMode = newOperatorMode;
+    updateUI();
 }
 
 function onNumberClicked(valueClicked) {
@@ -130,6 +243,64 @@ function onNumberClicked(valueClicked) {
 }
 
 /**
+ * Function that determines what to do with various key events
+ * 
+ * @param {KeyboardEvent} e - Keyboard event from the key press
+ */
+function onKeyPress(e) {
+    switch (e.key) {
+        case "0":
+            onNumberClicked(ValueClicked.ZERO);
+            break;
+         case "1":
+            onNumberClicked(ValueClicked.ONE);
+            break;
+        case "2":
+            onNumberClicked(ValueClicked.TWO);
+            break;
+        case "3":
+            onNumberClicked(ValueClicked.THREE);
+            break;
+        case "4":
+            onNumberClicked(ValueClicked.FOUR);
+            break;
+        case "5":
+            onNumberClicked(ValueClicked.FIVE);
+            break;
+        case "6":
+            onNumberClicked(ValueClicked.SIX);
+            break;
+        case "7":
+            onNumberClicked(ValueClicked.SEVEN);
+            break;
+        case "8":
+            onNumberClicked(ValueClicked.EIGHT);
+            break;
+        case "9":
+            onNumberClicked(ValueClicked.NINE);
+            break;
+        case "+":
+            onAddClick();
+            break;
+        case "-":
+            onSubtractClick();
+            break;
+        case "*":
+            onMultiplyClick();
+            break;
+        case "/":
+            onDivideClick();
+            break;
+        case "=":
+            onEqualClicked();
+            break;
+        default:
+            break;
+    }
+}
+
+
+/**
  * Appends a value to another value. 
  * If value to append is fractional it will be rounded down.
  * 
@@ -172,4 +343,20 @@ function appendNum(currentValue, value) {
  */
 function swapSign(value) {
     return value * -1;
+}
+
+function add(val, valToAdd) {
+    return val + valToAdd;
+}
+
+function multiply(val, valToMultiplyBy) {
+    return val * valToMultiplyBy;
+}
+
+function subtract(val, valToSubtractBy) {
+    return val - valToSubtractBy;
+}
+
+function divide(val, valToDivideBy) {
+    return val / valToDivideBy;
 }
